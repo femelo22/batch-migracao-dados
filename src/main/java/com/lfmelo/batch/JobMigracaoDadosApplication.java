b2c1e32bc10f726326c97b879339335faa220c75
femelo22 <luiz123jfmg@gmail.com>
2023-07-31 21:22:23 -0300

    feat: start project

---
+++ src/main/java/com/lfmelo/batch/JobMigracaoDadosApplication.java
@@ -0,0 +1,13 @@
+package com.lfmelo.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMigracaoDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMigracaoDadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lfmelo/batch/JobMigracaoDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfmelo.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMigracaoDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

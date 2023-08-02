187c8bce18166311588216f6c33748f2490656fd
femelo22 <luiz123jfmg@gmail.com>
2023-08-02 09:01:28 -0300

    feat: criar reader and writers

--- src/main/java/com/lfmelo/batch/job/MigracaoDadosJobConfig.java
+++ src/main/java/com/lfmelo/batch/job/MigracaoDadosJobConfig.java
@@ -2,12 +2,15 @@ package com.lfmelo.batch.job;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableBatchProcessing
 @Configuration
 public class MigracaoDadosJobConfig {
 	
@@ -15,7 +18,10 @@ public class MigracaoDadosJobConfig {
 	private JobBuilderFactory jobBuilderFactory;
 	
 	@Bean
-	public Job migracaoDadosJob(Step migrarPessoaStep, Step migrarDadosBancariosStep) {
+	public Job migracaoDadosJobConfig(
+			@Qualifier("migrarPessoaStep") Step migrarPessoaStep,
+			@Qualifier("migrarDadosBancariosStep") Step migrarDadosBancariosStep) {
+		
 		return jobBuilderFactory
 				.get("migracaoDadosJob")
 				.start(migrarPessoaStep)
---
+++ src/main/java/com/lfmelo/batch/reader/ArquivoDadosBancariosReader.java
@@ -0,0 +1,22 @@
+package com.lfmelo.batch.reader;
+
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import com.lfmelo.batch.domain.DadosBancarios;
+
+@Configuration
+public class ArquivoDadosBancariosReader {
+
+	@Bean
+	public FlatFileItemReader<DadosBancarios> arquivoDadosBancariosReader() {
+		return new FlatFileItemReaderBuilder<DadosBancarios>().name("arquivoDadosBancariosReader")
+				.resource(new FileSystemResource("files/dados_bancarios.csv")).delimited()
+				.names("pessoa_id", "agencia", "conta", "banco", "id").addComment("--").targetType(DadosBancarios.class)
+				.build();
+	}
+
+}
